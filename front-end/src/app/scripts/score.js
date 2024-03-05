40ff445912de011342455c388f4acc043d9c227f
ElouanBarois <elouan.barois@efpedu.fr>
2024-03-05 10:45:03 +0100

    Todo Classes: games, score, welcome

--- front-end/src/app/scripts/score.js
+++ front-end/src/app/scripts/score.js
@@ -1,23 +1,22 @@
 // TODO #import-html: use ES default imports to import game.html as template
 import template from "../views/score.html";
 import { parseUrl} from "./utils";
+import {Component} from "./components";
 
-  // TODO #class: use the ES6 class keyword
-  /* class ScoreComponent constructor */
-  export function ScoreComponent() {
-    // TODO #extends: call super(template)
-    var params = parseUrl();
-    this.template = template;
-    this.name = params.name;
-    this.size = parseInt(params.size);
-    this.time = parseInt(params.time);
-  }
+  export class ScoreComponent extends Component{
+    constructor() {
+      super(template);
+      var params = parseUrl();
+      this.name = params.name;
+      this.size = parseInt(params.size);
+      this.time = parseInt(params.time);
 
+    }
 
-  // TODO #class: turn function into a method of ScoreComponent
-  ScoreComponent.prototype.init = function init() {
+  init() {
 
     document.getElementById("name").innerText = this.name;
     document.getElementById("size").innerText = this.size.toString();
     document.getElementById("time").innerText = this.time.toString();
-  };
+  }
+  }
--- front-end/src/app/scripts/welcome.js
+++ front-end/src/app/scripts/welcome.js
@@ -1,42 +1,39 @@
 // TODO #import-html: use ES default imports to import welcome.html as template
 import template from "../views/welcome.html";
-  // TODO #class: use the ES6 class keyword
-  /* class WelcomeComponent constructor  */
-  export function WelcomeComponent() {
-    // TODO #extends: call super(template)
-    // TODO #import-html: assign template to this.template
-    this.template = template;
-  }
+import {Component} from "./components";
 
-  // TODO #class: turn function into a method of WelcomeComponent
-  /* method WelcomeComponent.init */
-  WelcomeComponent.prototype.init = function init() {
-    var form = document.querySelector("form.form-signin");
+  export class WelcomeComponent extends Component{
+    constructor() {
+        super(template);
+    }
 
-    form.addEventListener(
-      "submit",
-      // TODO #arrow-function: use arrow function instead.
-      function (event) {
-        event.preventDefault();
-        if (form.checkValidity() === false) {
-          event.stopPropagation();
-          form.classList.add("was-validated");
-        } else {
-          var name = event.srcElement.querySelector("#nickname").value;
-          var size = parseInt(event.srcElement.querySelector("#size").value);
 
-          _startGame(name, size);
-        }
-      }.bind(this),
-      false
-    );
+    init() {
+      var form = document.querySelector("form.form-signin");
 
-    return this;
-  };
+      form.addEventListener(
+          "submit",
+          // TODO #arrow-function: use arrow function instead.
+          function (event) {
+            event.preventDefault();
+            if (form.checkValidity() === false) {
+              event.stopPropagation();
+              form.classList.add("was-validated");
+            } else {
+              var name = event.srcElement.querySelector("#nickname").value;
+              var size = parseInt(event.srcElement.querySelector("#size").value);
 
-  // TODO #class: turn function into a method of WelcomeComponent
-  function _startGame(name, size) {
-    var gamePage = './#game';
-    // TODO #template-literals:  use template literals (backquotes)
-    window.location = gamePage + "?name=" + name + "&size=" + size;
+              this._startGame(name, size);
+            }
+          }.bind(this),
+          false
+      );
+
+      return this;
+    };
+     _startGame(name, size) {
+      var gamePage = './#game';
+      // TODO #template-literals:  use template literals (backquotes)
+      window.location = gamePage + "?name=" + name + "&size=" + size;
+    }
   }
